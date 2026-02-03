48b761424e0349c7de7efd0975bfcbb8bc8eed33
fufu <roycelang24@gmail.com>
2026-02-03 11:13:17 +0800

    chore: remove

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 
 from src.api.v1.router import router as v1_router
-from src.core.logging import configure_logging
 
 
 def create_app() -> FastAPI:
-    configure_logging()
 
     app = FastAPI(title="gitLLM", version="0.1.0")
     app.include_router(v1_router, prefix="/api/v1")
